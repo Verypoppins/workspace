06bda5fa2f0114537d04fd282169476c075b8f7f
Vera Rubio <verarubiogurpegui28@gmail.com>
2025-10-07 09:53:11 +0200

    movimiento de carpetas proyecto final

--- introduccion-programacion/03. Core-JavaScript/Mi proyecto/mi_proyecto_vera_Rubio.js
+++
@@ -1,5 +0,0 @@
-//EL JUEGO DEL AHORCADO
-//Prueba de hasta 3/5 palabras (usando clases para reutilizar las funciones)
-//Añadir un contador
-//Añadir una pista por cada palabra
-//Añadir una forma para que se vaya construyendo el dibujo
